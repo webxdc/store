940bbd15d32c26bb55cbb07cbcb3685020a2a2f1
septias <xxsebastian.kleahnxx@gmail.com>
2023-05-23 12:12:12 +0200

    typos

--- src/messages.rs
+++ src/messages.rs
@@ -3,14 +3,14 @@ use itertools::Itertools;
 pub fn creat_review_group_init_message(testers: &[String], publisher: &String) -> String {
     format!(
         r#"I created a new group to help you publish your app.
-The testers [{}] will test your app and the publisher [{publisher}] will eventually publish it to the appstore
+The testers [{}] will test your app and the publisher [{publisher}] will eventually publish it to the appstore.
 
-Your next step is to send me your bundled webxdc file (<your app>.xdc)"#,
+Your next step is to send me your bundled webxdc file. (<your app>.xdc)"#,
         testers.iter().join(",")
     )
 }
 
 pub fn appstore_message() -> &'static str {
     r#"Welcome to the appstore bot! 
-I will shortly send you the appstore itself wher you can explore new apps."#
+I will shortly send you the appstore itself, where you can explore new apps."#
 }
