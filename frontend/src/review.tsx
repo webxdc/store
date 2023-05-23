8453713ce246f36c2cc22c9e871957d7afb4455c
septias <xxsebastian.kleahnxx@gmail.com>
2023-05-23 11:54:17 +0200

    update styles

--- frontend/src/review.tsx
+++ frontend/src/review.tsx
@@ -23,27 +23,28 @@ const AppInfoPreview: Component<AppInfoPreviewProps> = (props) => {
             <Show when={!errors()[5]} fallback={
                 <div class="flex flex-col items-center">
                     <img src="https://via.placeholder.com/150" alt={props.appinfo.name} class="w-20 h-20 rounded-xl" />
-                    <p class="text-red">Please add a image.png to your webxdc bundle</p>
+                    <p class="text-red">Please add an image.png to your webxdc bundle</p>
                 </div>
             }>
                 <img src={"data:image/png;base64," + props.appinfo.image!} alt={props.appinfo.name} class="w-20 h-20 rounded-xl self-center" />
             </Show>
             <label>App Name</label>
-            <Show when={errors()[0]}><p class="text-red">You have to give a name</p></Show>
-            <input name="name" value={props.appinfo.name} onInput={handleInputChange} placeholder="App Name" />
+            <Show when={errors()[0]}><p class="text-red">You have to give a name.</p></Show>
+            <input class="mb-2" name="name" value={props.appinfo.name} onInput={handleInputChange} placeholder="App Name" />
             <label>Description</label>
-            <Show when={errors()[1]}><p class="text-red">You have to give a description</p></Show>
+            <Show when={errors()[1]}><p class="text-red">You have to give a description.</p></Show>
             <textarea name="description" value={props.appinfo.description} onInput={handleInputChange} placeholder="Description" />
             <label>Author</label>
-            <input name="author_name" value={props.appinfo.author_name} onInput={handleInputChange} placeholder="Author Name" />
+            <Show when={errors()[2]}><p class="text-red">You have to give an author name.</p></Show>
+            <input class="mb-2" name="author_name" value={props.appinfo.author_name} onInput={handleInputChange} placeholder="Author Name" />
             <label>Author Email</label>
-            <input name="author_email" value={props.appinfo.author_email || ''} disabled placeholder="Author Email" />
+            <input class="mb-2" name="author_email" value={props.appinfo.author_email || ''} disabled placeholder="Author Email" />
             <label>Source code url </label>
-            <Show when={errors()[4]}><p class="text-red">Please specify a source code url in your manifest.toml</p></Show>
-            <input name="source_code_url" value={props.appinfo.source_code_url || ''} disabled placeholder="Source Code URL" />
+            <Show when={errors()[4]}><p class="text-red">Please specify a source code url in your manifest.toml.</p></Show>
+            <input class="mb-2" name="source_code_url" value={props.appinfo.source_code_url || ''} disabled placeholder="Source Code URL" />
             <label>Version</label>
-            <Show when={errors()[4]}><p class="text-red">Please specify a version in your manifest.toml</p></Show>
-            <input name="version" value={props.appinfo.version || ''} disabled placeholder="Version" />
+            <Show when={errors()[6]}><p class="text-red">Please specify a version in your manifest.toml.</p></Show>
+            <input class="mb-2" name="version" value={props.appinfo.version || ''} disabled placeholder="Version" />
         </form>
     )
 }
@@ -64,22 +65,21 @@ const ReviewState: Component<ReviewStateProps> = (props) => {
         const target = e.target as HTMLInputElement;
         const name = target.name;
         const value = target.checked;
-
         props.setTestStatus({ ...props.testStatus, [name]: value });
     };
 
     return (
         <form class="flex flex-col gap-2 p-4 rounded shadow max-width bg-white border">
             <label class="flex items-center">
-                <input type="checkbox" name='android' checked={props.testStatus.android} onClick={handleInputChange} />
+                <input class="mb-2" type="checkbox" name='android' checked={props.testStatus.android} onClick={handleInputChange} />
                 <span class="ml-2">Works on Android</span>
             </label>
             <label class="flex items-center">
-                <input type="checkbox" name='ios' checked={props.testStatus.ios} onClick={handleInputChange} />
+                <input class="mb-2" type="checkbox" name='ios' checked={props.testStatus.ios} onClick={handleInputChange} />
                 <span class="ml-2">Works on iOS</span>
             </label>
             <label class="flex items-center">
-                <input type="checkbox" name='desktop' checked={props.testStatus.desktop} onClick={handleInputChange} />
+                <input class="mb-2" type="checkbox" name='desktop' checked={props.testStatus.desktop} onClick={handleInputChange} />
                 <span class="ml-2">Works on Desktop</span>
             </label>
         </form>
@@ -113,11 +113,11 @@ const AppDetail: Component = () => {
     if (import.meta.env.DEV) {
         setAppInfo({
             name: "Poll",
-            description: "poll app",
-            author_name: "xxde@you.de",
-            author_email: "uditaenr",
+            description: "Poll app where you can create crazy cool polls. This is a very long description for the pepe.",
+            author_name: "Jonas Arndt",
+            author_email: "xxde@you.de",
             source_code_url: "https://example.com",
-            image: "",
+            image: "a",
             version: "1.11",
         });
     }
@@ -126,36 +126,38 @@ const AppDetail: Component = () => {
     const is_testing_complete = createMemo(() => testStatus().android && testStatus().ios && testStatus().desktop)
     const is_complete = createMemo(() => is_appdata_complete() && is_testing_complete())
 
-
     function submit() {
-    }
 
+    }
 
     return (
         <div class="c-grid m-4">
-            <Show when={appInfo() !== undefined} fallback={
-                <p>Waiting for setup message...</p>
-            }>
-                <AppInfoPreview appinfo={appInfo()} setAppInfo={setAppInfo} />
-                <div>
+            <div class="min-width flex flex-col gap-3">
+                <h1 class="text-2xl text-center font-bold text-indigo-500"> App Publishing statatus</h1>
+                <Show when={appInfo() !== undefined} fallback={
+                    <p>Waiting for setup message...</p>
+                }>
+                    <AppInfoPreview appinfo={appInfo()} setAppInfo={setAppInfo} />
                     <p class="text-gray-500 font-italic">Testing Status</p>
-                    <ReviewState testStatus={testStatus()} setTestStatus={setTestStatus} />
-                </div>
-                <p class="text-gray-500 font-italic">
-                    <Show when={!is_complete()} fallback={
-                        <p>Ready for publishing!</p>
-                    }>
-                        <Show when={!is_appdata_complete()}>
-                            <p>Some app data is still missing</p>
+                    <div>
+                        <ReviewState testStatus={testStatus()} setTestStatus={setTestStatus} />
+                    </div>
+                    <p class="text-gray-500 font-italic">
+                        <Show when={!is_complete()} fallback={
+                            <p>Ready for publishing!</p>
+                        }>
+                            <Show when={!is_appdata_complete()}>
+                                <p>TODO: Some app data is still missing</p>
+                            </Show>
+                            <Show when={!is_testing_complete()}>
+                                <p>TODO: Testing is incomplete</p>
+                            </Show>
                         </Show>
-                        <Show when={!is_testing_complete()}>
-                            <p>Testing is incomplete</p>
-                        </Show>
-                    </Show>
-                </p>
-                <button class="btn font-semibold text-gray-500" classList={{ "bg-gray-100 border-gray-500": !is_complete() }}
-                    disabled={is_complete()} onClick={submit}>Submit</button>
-            </Show>
+                    </p>
+                    <button class="btn font-semibold text-indigo-500 w-full" classList={{ "bg-gray-100 border-gray-500": !is_complete() }}
+                        disabled={is_complete()} onClick={submit}>Submit</button>
+                </Show>
+            </div>
         </div>
     )
 };
--- frontend/src/submit.tsx
+++ frontend/src/submit.tsx
@@ -21,13 +21,13 @@ const AppSubmitForm: ComponentProps<any> = (props: any) => {
         let errors: Errors = {}
 
         if (appName() == '') {
-            errors['appName'] = 'You have to give an app name'
+            errors['appName'] = 'You have to give an app name.'
         }
         if (description() == '') {
-            errors['description'] = 'You have to give a description'
+            errors['description'] = 'You have to give a description.'
         }
         if (!hasRights()) {
-            errors['hasRights'] = 'You need to confirm that you own all the rights to this software'
+            errors['hasRights'] = 'You need to confirm that you own all the rights to this software.'
         }
 
         if (Object.keys(errors).length > 0) {
@@ -64,11 +64,11 @@ const AppSubmitForm: ComponentProps<any> = (props: any) => {
                 <label class="block text-gray-700"> Description </label>
                 <Show when={errors().description}><p class="text-red font-normal">{errors().description}</p></Show>
                 <textarea class="mt-1 block w-full mb-2" onInput={(e: any) => setDescription(e.target.value)}></textarea>
+                <Show when={errors().hasRights}><p class="text-red font-normal">{errors().hasRights}</p></Show>
                 <div class="flex items-center gap-2 mb-2">
                     <input type="checkbox" class="border-indigo-500" onClick={() => setHasRights(!hasRights())} />
                     <label class="block text-gray-700"> I have all the rights to this software </label>
                 </div>
-                <Show when={errors().hasRights}><p class="text-red font-normal">{errors().hasRights}</p></Show>
                 <button class="btn w-full mt-5">Submit</button>
             </form>
         </div>
