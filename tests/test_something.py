98771c29dff9ddf8d41acd8eda2317c65e73f77d
link2xt <link2xt@testrun.org>
2023-05-31 17:06:02 +0000

    Add python tests

---
+++ tests/test_something.py
@@ -0,0 +1,42 @@
+from subprocess import Popen
+from pathlib import Path
+from dataclasses import dataclass
+import shutil
+
+import pytest
+
+class BotProcess:
+    addr: str
+    process: Popen
+
+    def __init__(self, addr, password, path):
+        self.addr = addr
+        self.path = path
+
+        # Copy appstore.xdc to the bot working directory.
+        shutil.copy("./appstore.xdc", path)
+        self.process = Popen(
+            [Path.cwd() / "target/debug/github-bot", "start"],
+            cwd=path,
+            env={"addr": addr, "mail_pw": password, "RUST_LOG": "github_bot=trace"},
+        )
+
+    def __del__(self):
+        self.process.terminate()
+
+@pytest.fixture
+def storebot(acfactory, tmp_path):
+    config = acfactory.get_next_liveconfig()
+
+    return BotProcess(config["addr"], config["mail_pw"], tmp_path)
+
+
+def test_welcome_message(acfactory, storebot):
+    ac1, = acfactory.get_online_accounts(1)
+
+    bot_contact = ac1.create_contact(storebot.addr)
+    bot_chat = bot_contact.create_chat()
+    bot_chat.send_text("hi!")
+
+    msg_in = ac1.wait_next_incoming_message()
+    assert msg_in.text == "Welcome to the appstore bot!"
