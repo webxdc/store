4384f46d6d2097b6afd099d1646edc47a80c4599
link2xt <link2xt@testrun.org>
2023-06-03 12:08:52 +0000

    test: test update requests

--- tests/test_something.py
+++ tests/test_something.py
@@ -32,7 +32,8 @@ def storebot(acfactory, tmp_path):
 
 
 def test_welcome_message(acfactory, storebot):
-    ac1, = acfactory.get_online_accounts(1)
+    """Test that the bot responds with a welcome message."""
+    (ac1,) = acfactory.get_online_accounts(1)
 
     bot_contact = ac1.create_contact(storebot.addr)
     bot_chat = bot_contact.create_chat()
@@ -40,3 +41,34 @@ def test_welcome_message(acfactory, storebot):
 
     msg_in = ac1.wait_next_incoming_message()
     assert msg_in.text == "Welcome to the appstore bot!"
+
+
+def test_update(acfactory, storebot):
+    """Test that the bot sends initial update and responds to update requests."""
+    (ac1,) = acfactory.get_online_accounts(1)
+
+    bot_contact = ac1.create_contact(storebot.addr)
+    bot_chat = bot_contact.create_chat()
+    bot_chat.send_text("hi!")
+
+    msg_in = ac1.wait_next_incoming_message()
+    ac1._evtracker.get_matching("DC_EVENT_WEBXDC_STATUS_UPDATE")
+    assert msg_in.text == "Welcome to the appstore bot!"
+
+    assert msg_in.is_webxdc()
+    status_updates = msg_in.get_status_updates()
+    assert len(status_updates) == 1
+    assert status_updates[0]["payload"] == {"app_infos": [], "serial": 0}
+
+    # Request updates.
+    assert msg_in.send_status_update(
+        {"payload": {"request_type": "Update", "data": 0}}, "update"
+    )
+    ac1._evtracker.get_matching("DC_EVENT_WEBXDC_STATUS_UPDATE")
+
+    # Receive a response.
+    ac1._evtracker.get_matching("DC_EVENT_WEBXDC_STATUS_UPDATE")
+    status_updates = msg_in.get_status_updates()
+    assert len(status_updates) == 3
+    payload = status_updates[-1]["payload"]
+    assert payload == {"app_infos": [], "serial": 0}
