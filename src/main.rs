c874610f91cb2c3fb07848f09686f94a767a707f
Sebastian Klähn <scoreplayer2000@gmail.com>
2023-06-06 22:39:28 +0200

    configure the bot before any command can happen

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,12 @@ use bot::Bot;
 use clap::Parser;
 use cli::{BotActions, BotCli};
 use log::info;
-use sqlx::SqlitePool;
 use tokio::signal;
 
 use crate::request_handlers::AppInfo;
 
 const DB_URL: &str = "sqlite://bot-db/bot.db";
+const DC_DB_PATH: &str = "deltachat.db";
 const GENESIS_QR: &str = "./bot-data/genesis_invite_qr.png";
 const INVITE_QR: &str = "./bot-data/1o1_invite_qr.png";
 const SHOP_XDC: &str = "./bot-data/appstore.xdc";
@@ -31,6 +31,7 @@ const REVIEW_HELPER_XDC: &str = "./bot-data/review-helper.xdc";
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
     let cli = BotCli::parse();
+    let mut bot = Bot::new().await.context("failed to create bot")?;
 
     match &cli.action {
         BotActions::Import => {
@@ -85,8 +86,8 @@ async fn main() -> anyhow::Result<()> {
 
                         fs::rename(file, &new_path)?;
                         app_info.xdc_blob_dir = Some(new_path);
-                        let db = SqlitePool::connect(DB_URL).await?;
-                        db::create_app_info(&mut *db.acquire().await?, &mut app_info).await?;
+                        db::create_app_info(&mut *bot.get_db_connection().await?, &mut app_info)
+                            .await?;
                         println!("Added {:?}({}) to apps", file, app_info.name);
                     } else {
                         println!(
@@ -98,21 +99,16 @@ async fn main() -> anyhow::Result<()> {
                 }
             }
         }
-        BotActions::ShowQr => {
-            let db = SqlitePool::connect(DB_URL).await?;
-
-            match db::get_config(&mut *db.acquire().await?).await {
-                Ok(config) => {
-                    println!("Genisis invite qr:");
-                    qr2term::print_qr(config.genesis_qr)?;
-                    println!("Bot invite qr:");
-                    qr2term::print_qr(config.invite_qr)?;
-                }
-                Err(_) => println!("Bot not configured yet, start the bot first."),
+        BotActions::ShowQr => match db::get_config(&mut *bot.get_db_connection().await?).await {
+            Ok(config) => {
+                println!("Genisis invite qr:");
+                qr2term::print_qr(config.genesis_qr)?;
+                println!("Bot invite qr:");
+                qr2term::print_qr(config.invite_qr)?;
             }
-        }
+            Err(_) => println!("Bot not configured yet, start the bot first."),
+        },
         BotActions::Start => {
-            let mut bot = Bot::new().await.context("failed to create bot")?;
             bot.start().await;
             signal::ctrl_c().await?;
         }
