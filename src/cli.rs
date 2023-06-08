a8985bd644c7c8e64f4a05a8068e23262d9c57ce
Sebastian Klähn <scoreplayer2000@gmail.com>
2023-06-08 21:08:38 +0200

    document cli

--- src/cli.rs
+++ src/cli.rs
@@ -9,7 +9,10 @@ pub struct BotCli {
 
 #[derive(Subcommand, Debug)]
 pub enum BotActions {
+    /// Start the bot.
     Start,
+    /// Import xdcs from './import/'.
     Import,
+    /// Show the 1:1-invite and genesis-invite qr code.
     ShowQr,
 }
