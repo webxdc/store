a81c76bb46292b0c8e5e3c0baf749e17480d233e
septias <xxsebastian.kleahnxx@gmail.com>
2023-06-18 13:43:35 +0200

    increase spacing in appinfo preview

--- frontend/src/components/AppInfo.tsx
+++ frontend/src/components/AppInfo.tsx
@@ -23,33 +23,33 @@ const AppInfoPreview: Component<AppInfoPreviewProps> = (props) => {
   ))
 
   return (
-        <form class="max-width flex flex-col border rounded bg-white p-4 shadow">
-            <Show when={!errors().image} fallback={
-                <div class="flex flex-col items-center">
-                    <img src="150.png" alt={props.appinfo.name} class="h-20 w-20 rounded-xl" />
-                    <p class="text-red">Please add an image.png to your webxdc bundle</p>
-                </div>
-            }>
-                <img src={`data:image/png;base64,${props.appinfo.image!}`} alt={props.appinfo.name} class="h-20 w-20 self-center rounded-xl" />
-            </Show>
-            <label>App Name</label>
-            <Show when={errors().name}><p class="text-red">You have to give a name.</p></Show>
-            <input class="mb-2" name="name" value={props.appinfo.name} onInput={handleInputChange} disabled={props.disable_all} placeholder="App Name" />
-            <label>Description</label>
-            <Show when={errors().description}><p class="text-red">You have to give a description.</p></Show>
-            <textarea name="description" value={props.appinfo.description || ''} onInput={handleInputChange} disabled={props.disable_all} placeholder="Description" />
-            <label>Author</label>
-            <Show when={errors().author_name}><p class="text-red">You have to give an author name.</p></Show>
-            <input class="mb-2" name="author_name" value={props.appinfo.author_name} onInput={handleInputChange} disabled={props.disable_all} placeholder="Author Name" />
-            <label>Author Email</label>
-            <input class="mb-2" name="author_email" value={props.appinfo.author_email || ''} disabled placeholder="Author Email" />
-            <label>Source code url </label>
-            <Show when={errors().source_code_url}><p class="text-red">Please specify a source code url in your manifest.toml.</p></Show>
-            <input class="mb-2" name="source_code_url" value={props.appinfo.source_code_url || ''} disabled placeholder="Source Code URL" />
-            <label>Version</label>
-            <Show when={errors().version}><p class="text-red">Please specify a version in your manifest.toml.</p></Show>
-            <input class="mb-2" name="version" value={props.appinfo.version || ''} disabled placeholder="Version" />
-        </form>
+    <form class="max-width flex flex-col border rounded bg-white p-4 shadow">
+      <Show when={!errors().image} fallback={
+        <div class="flex flex-col items-center">
+          <img src="150.png" alt={props.appinfo.name} class="h-20 w-20 rounded-xl" />
+          <p class="text-red">Please add an image.png to your webxdc bundle</p>
+        </div>
+      }>
+        <img src={`data:image/png;base64,${props.appinfo.image!}`} alt={props.appinfo.name} class="h-20 w-20 self-center rounded-xl" />
+      </Show>
+      <label>App Name</label>
+      <Show when={errors().name}><p class="text-red">You have to give a name.</p></Show>
+      <input class="mb-3" name="name" value={props.appinfo.name} onInput={handleInputChange} disabled={props.disable_all} placeholder="App Name" />
+      <label>Description</label>
+      <Show when={errors().description}><p class="text-red">You have to give a description.</p></Show>
+      <textarea class="mb-3" name="description" value={props.appinfo.description || ''} onInput={handleInputChange} disabled={props.disable_all} placeholder="Description" />
+      <label>Author</label>
+      <Show when={errors().author_name}><p class="text-red">You have to give an author name.</p></Show>
+      <input class="mb-3" name="author_name" value={props.appinfo.author_name} onInput={handleInputChange} disabled={props.disable_all} placeholder="Author Name" />
+      <label>Author Email</label>
+      <input class="mb-3" name="author_email" value={props.appinfo.author_email || ''} disabled placeholder="Author Email" />
+      <label>Source code url </label>
+      <Show when={errors().source_code_url}><p class="text-red">Please specify a source code url in your manifest.toml.</p></Show>
+      <input class="mb-3" name="source_code_url" value={props.appinfo.source_code_url || ''} disabled placeholder="Source Code URL" />
+      <label>Version</label>
+      <Show when={errors().version}><p class="text-red">Please specify a version in your manifest.toml.</p></Show>
+      <input class="mb-3" name="version" value={props.appinfo.version || ''} disabled placeholder="Version" />
+    </form>
   )
 }
 
