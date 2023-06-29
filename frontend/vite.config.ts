2f0b7a8168a1784bce3dbda2fb5f6ecc6f12740a
Sebastian Klähn <scoreplayer2000@gmail.com>
2023-06-29 14:28:07 +0200

    add version

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,30 +1,33 @@
-import { defineConfig, loadEnv } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import unocssPlugin from "unocss/vite"
-//@ts-ignore
-import { resolve } from 'path'
+import { resolve } from 'node:path'
+import { execSync } from 'child_process'
+import { defineConfig, loadEnv } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+import unocssPlugin from 'unocss/vite'
+
+// @ts-expect-error
 
 export default defineConfig(({ mode }) => {
-  //@ts-ignore
+  // @ts-expect-error
   const env = loadEnv(mode, process.cwd(), '')
-  const app_name = env.VITE_APP || 'shop'
+  const app_name = env.VITE_APP
+  process.env.VITE_COMMIT = String(execSync('git describe --always'))
 
   return {
     plugins: [solidPlugin(), unocssPlugin()],
     build: {
-      target: ["es2020", "edge88", "firefox78", "chrome74", "safari14"],
+      target: ['es2020', 'edge88', 'firefox78', 'chrome74', 'safari14'],
       rollupOptions: {
         input: {
-          //@ts-ignore
+          // @ts-expect-error
           shop: resolve(__dirname, `./${app_name}.html`),
         },
         output: {
-          dir: 'dist/' + app_name
-        }
+          dir: `dist/${app_name}`,
+        },
       },
     },
     server: {
       port: 3000,
-    }
+    },
   }
-});
+})
