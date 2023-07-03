190a948bf5be46a83f04c21da8050a5ab737b55a
septias <scoreplayer2000@gmail.com>
2023-07-04 00:18:18 +0200

    remove submission system from frontend

--- frontend/create_xdc.sh
+++ frontend/create_xdc.sh
@@ -8,25 +8,7 @@ mkdir -p "$DESTDIR"
 cd dist
 
 echo "Building store.xdc"
-cd shop
-mv shop.html index.html
-cp ../../build-files/shop/* .
+cp ../build-files/shop/* .
 zip -9 --recurse-paths "store.xdc" *
 cp store.xdc "$DESTDIR"
-cd ..
-
-echo "Building submit-helper.xdc"
-cd submit
-mv submit.html index.html
-cp ../../build-files/submit/* .
-zip -9 --recurse-paths "submit-helper.xdc" * 
-cp submit-helper.xdc "$DESTDIR"
-cd ..
-
-echo "Building review-helper.xdc"
-cd review
-mv review.html index.html
-cp ../../build-files/review/* .
-zip -9 --recurse-paths "review-helper.xdc" * 
-cp review-helper.xdc "$DESTDIR"
 cd ..
