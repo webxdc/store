def7220038d7f53d0f27be9d78786af3b5507fa1
adbenitez <asieldbenitez@gmail.com>
2023-07-04 15:42:26 +0200

    use pnpm directly

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -7,13 +7,13 @@ SRC="$PWD"
 
 # Build the frontend.
 cd "$SRC/frontend"
-npx pnpm install
+pnpm install
 
 TMP="$(mktemp -d)"
 export DESTDIR="$TMP/xdcstore"
 mkdir "$DESTDIR"
 
-npx pnpm build
+pnpm build
 
 # Build the backend.
 cd "$SRC"
