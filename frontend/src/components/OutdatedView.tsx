4bc1368ca1f12ac831b8b22a44dcfae43850e685
Sebastian Klähn <scoreplayer2000@gmail.com>
2023-07-10 12:43:32 +0200

    finalize

--- frontend/src/components/OutdatedView.tsx
+++ frontend/src/components/OutdatedView.tsx
@@ -2,9 +2,6 @@ import { type Component, Show, createSignal } from 'solid-js'
 import type { GeneralFrontendRequest } from '../bindings/GeneralFrontendRequest'
 
 interface OutdatedViewProps {
-  // If critical, the webxdc should stop working.
-  critical: boolean
-  children: any
   updated_received: boolean
 }
 
@@ -18,27 +15,26 @@ const AppInfoPreview: Component<OutdatedViewProps> = (props) => {
     setButtonUsed(true)
   }
   return (
-    <>
-      <div classList={{ 'blur overflow-hidden h-screen': props.critical }}> {props.children} </div>
-      {props.critical
-        && <div class="absolute right-0 top-0 grid h-screen w-screen place-content-center p-2">
-          <div class="flex flex-col gap-2 border border-red rounded bg-white p-2">
-            <h1 class='text-center font-bold text-red-700'> Outdated Version </h1>
-            <p> A newer version of this webxdc is available. </p>
-            {!buttonUsed() && <button class="self-center btn" onclick={update_req}> Download </button>}
-            {buttonUsed()
-              && <p class="self-center unimportant">
-                <Show when={props.updated_received} fallback={
-                  'Downloading..'
-                }>
-                  Update received in chat
-                </Show>
-              </p>
-            }
-          </div>
-        </div>
-      }
-    </>
+    <div class="absolute right-0 top-0 grid h-screen w-screen place-content-center p-2">
+      <div class="flex flex-col gap-2 border border-red-700 rounded-xl bg-white p-4">
+        <h1 class='text-center font-bold text-red-700'> Outdated Version </h1>
+        <p> A newer version of the store is available. </p>
+        <Show when={!buttonUsed()} fallback={
+          <Show when={props.updated_received} fallback={
+            <div class="self-center btn">
+
+              <div class="loading-spinner border border-blue-500 rounded" i-eos-icons-loading></div>
+            </div>
+          }>
+            <p class="self-center unimportant">
+              Update received in chat
+            </p>
+          </Show>
+        }>
+          <button class="self-center btn" onclick={update_req}> Download </button>
+        </Show>
+      </div>
+    </div>
   )
 }
 
