3952356f04f5d361879a41e9e3465ec53b2fe82d
link2xt <link2xt@testrun.org>
2023-07-29 18:27:33 +0000

    feat: set default log level to "info"

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ const VERSION: &str = include_file_str!("VERSION");
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    env_logger::init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let cli = BotCli::parse();
 
     match &cli.action {
